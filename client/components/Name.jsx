78057bd605a949b2765374a71eeb7d6b1bc45af9
anna-li-22 <111815022+anna-li-22@users.noreply.github.com>
2022-11-20 22:49:42 +1300

    css updates

--- client/components/Name.jsx
+++ client/components/Name.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addName } from '../actions'
-import { Link } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
 function Name() {
   const [newNameOne, setNewNameOne] = useState('')
   const [newNameTwo, setNewNameTwo] = useState('')
-
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   // const navigate = useNavigate()
 
@@ -17,7 +16,7 @@ function Name() {
   // }, [])
   function handleChange(event) {
     event.preventDefault()
-    // console.log(event.target.value)
+    console.log(event.target.value)
     setNewNameOne(event.target.value)
   }
   function handleChangeTwo(event) {
@@ -32,6 +31,7 @@ function Name() {
         NameTwo: newNameTwo,
       })
     )
+    navigate('/poem')
     console.log(newNameOne)
     event.preventDefault()
   }
@@ -41,35 +41,37 @@ function Name() {
       <div className="name_section">
         <div className="content_container">
           <p className="head_title">LOVE YOU TO THE MOON AND BACK</p>
-          <form>
-            <div className="input">
-              <label type="submit" htmlFor="name1">
-                From
-              </label>
-              <input
-                type="text"
-                aria-label="NewFoxName"
-                value={newNameOne}
-                onChange={handleChange}
-                className="name1"
-              ></input>
-            </div>
-            <div className="input">
-              <label htmlFor="name2">To</label>
-              <input
-                type="text"
-                aria-label="NewFoxName"
-                value={newNameTwo}
-                onChange={handleChangeTwo}
-                className="name2"
-                size="10"
-              ></input>
-            </div>
-            <br></br>
-            <Link to="/poem">
-              <button onClick={handleSubmit} className="submit"></button>
-            </Link>
-          </form>
+          <div className="form_container">
+            <form>
+              <div className="input">
+                <label type="submit" htmlFor="name1">
+                  From
+                </label>
+                <input
+                  type="text"
+                  aria-label="NewFoxName"
+                  value={newNameOne}
+                  onChange={handleChange}
+                  className="name1"
+                ></input>
+              </div>
+              <div className="input">
+                <label htmlFor="name2">To</label>
+                <input
+                  type="text"
+                  aria-label="NewFoxName"
+                  value={newNameTwo}
+                  onChange={handleChangeTwo}
+                  className="name2"
+                  size="10"
+                ></input>
+              </div>
+
+              <Link to="/poem">
+                <button onClick={handleSubmit} className="submit"></button>
+              </Link>
+            </form>
+          </div>
         </div>
         <div className="img_container">
           <img src="/images/moonHD.png" alt="Moon" />
